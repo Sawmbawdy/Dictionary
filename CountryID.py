b2fdc34e6ee6ee9a7ba6b33c2b702127ab483730
Sawmbawdy <saiompatro12@gmail.com>
2025-10-04 18:54:14 -0400

    Add files via upload

---
+++ CountryID.py
@@ -0,0 +1,3 @@
+Countries = {'India': 91, 'USA': 1, 'China': 86, 'Russia': 7}
+print("India's Country code is", Countries['India'])
+print("USA's Country code is", Countries.get('Japan', 'Not found'))
